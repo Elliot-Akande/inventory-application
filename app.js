ceb8730c9b3c5a5aba58202f2710c84e186681ac
Elliot Akande <elbo.lynch@gmail.com>
2023-12-14 11:50:52 +0000

    Install and configure helmet

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 
 const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
